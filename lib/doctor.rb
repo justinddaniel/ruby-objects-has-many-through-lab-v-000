02b346452bfb6cfba1cdb820f1b0a99ac8684cc9
Justin Daniel <justinddaniel@gmail.com>
2017-09-28 15:47:09 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,19 @@
+class Doctor
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    self.appointments << appointment
+    appointment.doctor = self
+  end
+
+  def patients
+    self.appointments.collect |a|
+    a.patient 
+  end
+end
+
+end
